7a29cbf3fb7f642931c45fb4a646dcb413feef7a
radonn24 <richardadonnell@gmail.com>
2023-09-21 03:49:09 -0500

    updated args, and added VSCode Workspace

--- webui-user.bat
+++ webui-user.bat
@@ -1,10 +1,11 @@
 @REM List of commandline args: https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Command-Line-Arguments-and-Settings#all-command-line-arguments
+@REM Saved cmd arg snippet: --theme dark
 
 @echo off
 
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --allow-code --upcast-sampling --xformers --precision full --skip-torch-cuda-test --no-half-vae --opt-split-attention --autolaunch --api --theme dark
+set COMMANDLINE_ARGS= --allow-code --upcast-sampling --xformers --precision full --skip-torch-cuda-test --no-half-vae --opt-split-attention --autolaunch --api
 
 call webui.bat
